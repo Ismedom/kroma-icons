9680e7557afbc8d63ff80bf270a0275f959aab66
ismedom <Khavudom.123@gmail.com>
2024-11-13 18:36:26 +0700

    testing

--- components/ModalDetailIcon.tsx
+++ components/ModalDetailIcon.tsx
@@ -3,13 +3,13 @@ import React from "react";
 import { Button } from "./ui/button";
 import downloadSvgFile from "@/functions/downloadSvg";
 import copySvg from "@/functions/copySvgText";
-import dynamic from "next/dynamic";
+// import dynamic from "next/dynamic";
 
-const LoadingSpinner = () => (
-    <div className="flex justify-center items-center">
-        <div className="animate-spin rounded-full h-8 w-8 border-t-2 border-b-2 border-gray-700"></div>
-    </div>
-);
+// const LoadingSpinner = () => (
+//     <div className="flex justify-center items-center">
+//         <div className="animate-spin rounded-full h-8 w-8 border-t-2 border-b-2 border-gray-700"></div>
+//     </div>
+// );
 
 const ModalDetailIcon = ({
     id,
